c599c3f7e7f03393117c0b1dad89b001d9f5b994
Unknown <rrooxxaass@hotmail.fr>
2018-02-23 16:24:24 +0100

    Class Scenario

--- app/src/main/java/m1project/remoteandroid/model/Scenario.java
+++ app/src/main/java/m1project/remoteandroid/model/Scenario.java
@@ -7,7 +7,91 @@ import java.util.ListIterator;
  */
 
 public class Scenario {
+    Ecran video;
+    String file;
+    List screens;
+    boolean distributed;
+    boolean mute;
+    boolean state;
+    boolean loop;
+    int volume;
+    Float departure;
 
+    public Scenario(Ecran video, String file, List screens, boolean distributed) {
+        this.video = video;
+        this.file = file;
+        this.screens = screens;
+        this.distributed = distributed;
+        this.mute = false;
+        this.state = true;
+        this.loop = false;
+        this.volume = 10;
+        this.departure = 0.0;
+    }
 
+    public String getFile() {
+        return file;
+    }
 
+    public void setFile(String file) {
+        this.file = file;
+    }
+
+    public List getScreens() {
+        return screens;
+    }
+
+    public void setScreens(List screens) {
+        this.screens = screens;
+    }
+
+    public boolean isDistributed() {
+        return distributed;
+    }
+
+    public void setDistributed(boolean distributed) {
+        this.distributed = distributed;
+    }
+
+    public boolean isMute() {
+        return mute;
+    }
+
+    public void setMute(boolean mute) {
+        this.mute = mute;
+    }
+
+    public boolean isState() {
+        return state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
+
+    public boolean isLoop() {
+        return loop;
+    }
+
+    public void setLoop(boolean loop) {
+        this.loop = loop;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public Float getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(Float departure) {
+        this.departure = departure;
+    }
 }
+
+
