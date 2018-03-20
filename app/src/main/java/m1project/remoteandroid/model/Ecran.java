ec5187c8fca6dffebbb9dc22a67bae5a7bcecde9
Unknown <rrooxxaass@hotmail.fr>
2018-03-20 16:19:22 +0100

    revert

--- app/src/main/java/m1project/remoteandroid/model/Ecran.java
+++ app/src/main/java/m1project/remoteandroid/model/Ecran.java
@@ -10,8 +10,6 @@ public class Ecran {
     private String type;
     private String ipv4;
     private String resolution;
-    private int row;
-    private int cols;
     private boolean tactile;
     private boolean orientation;
 
@@ -52,22 +50,6 @@ public class Ecran {
 
     public void setResolution(String resolution) {
         this.resolution = resolution;
-    }
-
-    public int getRow() {
-        return row;
-    }
-
-    public void setRow(int row) {
-        this.row = row;
-    }
-
-    public int getCols() {
-        return cols;
-    }
-
-    public void setCols(int cols) {
-        this.cols = cols;
     }
 
     public boolean isTactile() {
--- app/src/main/java/m1project/remoteandroid/model/Scenario.java
+++ app/src/main/java/m1project/remoteandroid/model/Scenario.java
@@ -1,102 +1,50 @@
 package m1project.remoteandroid.model;
-import java.util.LinkedList;
+
 import java.util.List;
-import java.util.ListIterator;
 /**
+
  * Created by Zephyr on 23/02/2018.
+
  */
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Scenario {
-    Ecran video;
-    String file;
-    List screens;
-    boolean distributed;
-    boolean mute;
-    boolean state;
-    boolean loop;
-    int volume;
-    Float departure;
+         Map<String,ArrayList<Integer> > ecranpos;
+         ArrayList video;
+         String name;
 
-    // scenario = video
-    // creer new class  Scenario qui contient liste video ( voir json prof)
+    public Scenario(ArrayList video, String name,HashMap ecranpos) {
+                 this.ecranpos = new HashMap<>();
+                 this.ecranpos.putAll(ecranpos);
+                 this.video=new ArrayList();
+                 this.video= (ArrayList)video.clone();
+                 this.name = name;
+    }
 
-    public Scenario(Ecran video, String file, List screens, boolean distributed) {
+    public Map<String, ArrayList<Integer>> getEcranpos() {
+        return ecranpos;
+    }
+
+    public void setEcranpos(Map<String, ArrayList<Integer>> ecranpos) {
+        this.ecranpos = ecranpos;
+    }
+
+    public ArrayList getVideo() {
+        return video;
+    }
+
+    public void setVideo(ArrayList video) {
         this.video = video;
-        this.file = file;
-        this.screens = screens;
-        this.distributed = distributed;
-        this.mute = false;
-        this.state = true;
-        this.loop = false;
-        this.volume = 10;
-        //this.departure = 0.0;
-
-        //liste screen : cle valeur : id ecran, position
     }
 
-    public String getFile() {
-        return file;
+    public String getName() {
+        return name;
     }
 
-    public void setFile(String file) {
-        this.file = file;
-    }
-
-    public List getScreens() {
-        return screens;
-    }
-
-    public void setScreens(List screens) {
-        this.screens = screens;
-    }
-
-    public boolean isDistributed() {
-        return distributed;
-    }
-
-    public void setDistributed(boolean distributed) {
-        this.distributed = distributed;
-    }
-
-    public boolean isMute() {
-        return mute;
-    }
-
-    public void setMute(boolean mute) {
-        this.mute = mute;
-    }
-
-    public boolean isState() {
-        return state;
-    }
-
-    public void setState(boolean state) {
-        this.state = state;
-    }
-
-    public boolean isLoop() {
-        return loop;
-    }
-
-    public void setLoop(boolean loop) {
-        this.loop = loop;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-    public Float getDeparture() {
-        return departure;
-    }
-
-    public void setDeparture(Float departure) {
-        this.departure = departure;
+    public void setName(String name) {
+        this.name = name;
     }
 }
-
-
